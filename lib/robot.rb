c4dd48338b648a4aa914faa1f5751272cab77534
Vanessa Nimmo <vanessanimmo@live.com>
2018-11-10 14:30:17 +1100

    A few robot tests done and passing

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,2 +1,18 @@
 class Robot
+attr_accessor :x, :y, :facing
+attr_reader :placed
+ 
+  def initialize
+    @x
+    @y
+    @facing
+    @placed = false
+  end
+
+  def place(x, y, facing)
+    @placed = true
+    @x = x
+    @y = y
+    @facing = facing
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,6 +2,32 @@ require 'robot'
 
 RSpec.describe Robot do
   describe ".initialize" do
-    
+
+    it "should be an object of the class Robot" do
+      expect(subject).to be_a_kind_of(Robot)
+    end
+
+    it "should have x and y coordinates and a direction" do
+      expect(subject.x).to be(nil)
+      expect(subject.y).to be(nil)
+      expect(subject.facing).to be(nil)
+    end
+
+    it "should accept and execute a place command" do
+      subject.place(0,0,'NORTH')
+      expect(subject.x).to eq(0)
+      expect(subject.y).to eq(0)
+      expect(subject.facing).to eq('NORTH')
+    end
+
+    it "should know if it has been placed" do
+      subject.place(0,0,'NORTH')
+      expect(subject.placed).to eq(true)
+    end
+
+    it "should move forward one space in the direction it is facing when given the command MOVE" do
+      
+    end
+
   end
 end
